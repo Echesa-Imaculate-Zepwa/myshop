c788df46dfbaec6e4b9c23defef186557e4f782c
Echesa-Imaculate-Zepwa <110254581+Echesa-Imaculate-Zepwa@users.noreply.github.com>
2022-10-07 16:25:51 +0300

    Update SearchBar.js
    search function

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,37 @@
-import React from "react";
-const SearchBar = () => (
-    
-<>
-     <form >
-      <label>
-          <span>ITEMS HERE</span>
-      </label>
-      <input
-          type="text"
-          id="header-search"
-          placeholder="Search item"
-    
-      />
-      <button type="submit">Search</button>
-  </form>
-  </>
-);
+import React, { useEffect, useState } from "react";
+
+function SearchBar({products, setProducts}) {
+  const [ searchValue, setSearchValue ] = useState("")
+//   console.log(products[0])
+
+
+    useEffect(() => {
+      const filteredProducts = products.filter(product => {
+        const lowerProdDescription = product.name.toLowerCase()
+        return lowerProdDescription.includes(searchValue.toLowerCase())
+        })
+        setProducts(filteredProducts)
+    }, [searchValue]);
+
+    const onChange = (e) => setSearchValue(e.target.value)
+//   console.log(products)
+
+    return (<div>
+         <form >
+          <label>
+              <span>ITEMS HERE: </span>
+          </label>
+          <input
+              type="text"
+              id="header-search"
+              placeholder="Search item"
+              onChange={onChange}
+          />
+          <button type="submit">Search</button>
+      </form>
+    </div>)
+};
 
 export default SearchBar;
+                      
+                      
